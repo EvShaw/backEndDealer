2e2e49a3b03a69b4df3c4cfc2bd9bb76e3c00e36
Evan Shaw <e85.shaw@gmail.com>
2022-09-14 10:25:28 -0700

    add route, controller, view, add DB

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const app = express()
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 // const methodOverride = require('method-override')
-// const connectDB = require('./config/database')
+const connectDB = require('./config/database')
 const logger = require('morgan')
-
-//routes:
+const mainRoutes = require('./routes/main')
 
 //.env file in config
 require('dotenv').config({ path: './config/.env' })
 
 //connect to DB
-// connectDB()
+connectDB()
 
-//ejs for simple display
-//not implemented
+//rendering with EJS for views
+app.set('view engine', 'ejs')
 
 //static folder 
 //not implemented
@@ -24,6 +23,10 @@ app.use(logger('dev'))
 
 //use forms for put || deletes
 // app.use(methodOVerride('_method'))
+
+
+//Routes
+app.use('/', mainRoutes)
 
 
 app.listen(process.env.PORT, () => {
